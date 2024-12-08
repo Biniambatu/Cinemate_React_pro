aec06da821f66bcd0e109e1b7e3dd86e5e9013db
Biniam batu <Biniambatu12@gmail.com>
2024-12-09 00:36:21 +0300

    axios

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,17 +1,18 @@
+import axios from "axios";
 import { useState, useEffect } from "react";
 
 export const useFetch = (apiPath) => {
   
   const [data, setData] = useState([])
+  const [error, setError] = useState()
+  const [loading, setLoading] = useState(true);
   const url = `https://api.themoviedb.org/3/${apiPath}?api_key=4bad53a04ca878afc74cf970bdc369bd`
 
   useEffect(() => {
-    async function fetchMovies() {
-      const response = await fetch(url)
-      const json = await response.json()
-      setData(json.results)
-    }
-    fetchMovies()
+    axios.get(url)
+      .then(res => setData(res.data.results))
+      .catch(err => setError(err.message))
+      .finally(() => setLoading(true))
   },[url])
 
      return {data}
