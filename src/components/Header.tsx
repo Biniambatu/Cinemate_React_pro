155290ca3c30bc9606653ef72fb345194986416f
Biniam batu <Biniambatu12@gmail.com>
2024-12-09 12:32:39 +0300

    search page button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,6 +6,7 @@ export const Header = () => {
   const inActiveClass = "text-baseblock py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
   const[hidden, setHidden] =  useState(true)
   const [darkMode, setDarkMode] = useState( JSON.parse(localStorage.getItem("darkmode")) || false);
+  const navigate = useNavigate()
   useEffect(() => {
     localStorage.setItem("darkMode", JSON.stringify(darkMode));
     if(darkMode){
@@ -16,6 +17,13 @@ export const Header = () => {
     } 
 
   }, [darkMode]);
+  const handleSubmit = (event:any) => {
+    event.preventDefault()
+    const queryTerm = event.target.search.value
+    event.target.reset()
+
+    return navigate(`/search?q=${queryTerm}`)
+  }
   return (
     <header>
       <nav className="bg-white border-b-2 border-gray-200 dark:bg-gray-900 dark:border-b-1 dark:border-gary-900">
@@ -41,9 +49,11 @@ export const Header = () => {
                 <svg className="w-4 h-4 text-gray-500 dark:text-gray-400"aria-hidden="true"                  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20" >
                   <path stroke="currentColor"stroke-linecap="round" stroke-linejoin="round" stroke-width="2"d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/> 
                 </svg>
-                <span className="sr-only">Search icon</span>
+                 <span className="sr-only">Search icon</span>
               </div>
-              <input type="text"id="search-navbar" className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..." /> 
+              <form onSubmit={handleSubmit}>
+                <input type="text"id="search-navbar" name="search" className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..." /> 
+              </form>
             </div>
             <button onClick={()=>setHidden(!hidden)} data-collapse-toggle="navbar-search" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-search" aria-expanded="false">
               <span className="sr-only">Open main menu</span>
@@ -58,7 +68,9 @@ export const Header = () => {
               <path stroke="currentColor"stroke-linecap="round"  stroke-linejoin="round"  stroke-width="2"  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                 </svg>
               </div>
-              <input type="text"id="search-navbar" className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"placeholder="Search..." />
+              <form onSubmit={handleSubmit}>
+                <input type="text"id="search-navbar" name="search" className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"placeholder="Search..." />
+              </form>
             </div>
             <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -11,6 +11,9 @@ export const Search = ({apiPath}:any) => {
 
   return (
     <main>
+      <section className="py-7"> 
+        <p className="text-3xl text-gray-700 dark:text-white">{ movies.length === 0 ?  `No result found for ${queryTerm}` : `Result for ${queryTerm}` }</p>
+      </section>
       <main>
       <section className="max-w-7x1 mx-auto py-7">
         <div className="flex justify-start flex-wrap">
